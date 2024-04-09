fbd131a31f897dea9d9b0477249141b787d00313
Mahendra Kirana M.B <113653966+Kyyneko@users.noreply.github.com>
2024-04-09 08:07:50 +0800

    Update BlacklistToken.py

--- app/models/BlacklistToken.py
+++ app/models/BlacklistToken.py
@@ -1,6 +1,6 @@
 from app import db
 
 class BlacklistToken(db.Model):
-    __tablename__ = 'blacklisttoken'  # Sesuaikan nama tabel di sini
+    __tablename__ = 'blacklisttoken'
     id = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.String(500), unique=True, nullable=False)
